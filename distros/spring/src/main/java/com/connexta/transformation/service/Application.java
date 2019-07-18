d1fe6f4354d49c9f350a80da337caedb63f77bdc
figliold <daniel.figliola@connexta.com>
2019-07-18 11:43:17 -0700

    LORE-187: Update Transformation API Impl to use REST instead of queues. (#10)
    * LORE-187: Update Transformation API Impl to use REST instead of queues.
    
    * LORE-187: Remove produces from RequestMapping in TransformController
    
    * LORE-187: Added license header to application yaml files

--- distros/spring/src/main/java/com/connexta/transformation/service/Application.java
+++ distros/spring/src/main/java/com/connexta/transformation/service/Application.java
@@ -13,17 +13,12 @@
  */
 package com.connexta.transformation.service;
 
-import com.connexta.transformation.TransformationService;
-import com.connexta.transformation.service.TransformationServiceImpl.DeliveryConfirmCallback;
-import com.connexta.transformation.service.TransformationServiceImpl.DeliveryReturnCallback;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 /**
@@ -37,26 +32,8 @@ public class Application {
 
   private static final String INBOUND_REQUEST_PREFIX = "Inbound Request: ";
 
-  @Value("${spring.rabbitmq.host}")
-  private String messageBrokerHost;
-
-  @Value("${spring.rabbitmq.port}")
-  private int messageBrokerPort;
-
-  @Value("${spring.rabbitmq.virtualHost}")
-  private String virtualHost;
-
-  @Value("${spring.rabbitmq.user}")
-  private String user;
-
-  @Value("${spring.rabbitmq.password}")
-  private String password;
-
-  @Value("${spring.rabbitmq.exchange}")
-  private String exchange;
-
-  @Value("${spring.rabbitmq.routingKey}")
-  private String routingKey;
+  @Value("${endpoints.lookupService.url}")
+  private String lookupServiceUrl;
 
   @Bean
   public CommonsRequestLoggingFilter requestLoggingFilter() {
@@ -70,68 +47,14 @@ public class Application {
     return loggingFilter;
   }
 
-  @Bean(name = "exchange")
-  public String getExchange() {
-    return exchange;
-  }
-
-  @Bean(name = "routingKey")
-  public String getRoutingKey() {
-    return routingKey;
+  @Bean(name = "lookupServiceUrl")
+  public String getLookupServiceUrl() {
+    return lookupServiceUrl;
   }
 
   @Bean
-  public TransformationService transformationService(
-      RabbitTemplate rabbitTemplate, String exchange, String routingKey) {
-    return new TransformationServiceImpl(rabbitTemplate, exchange, routingKey);
-  }
-
-  @Bean
-  public TransformController transformController(TransformationService transformationService) {
-    return new TransformController(transformationService);
-  }
-
-  @Bean
-  public ConnectionFactory connectionFactory() {
-    final CachingConnectionFactory connectionFactory =
-        new CachingConnectionFactory(messageBrokerHost, messageBrokerPort);
-    connectionFactory.setUsername(user);
-    connectionFactory.setPassword(password);
-    connectionFactory.setVirtualHost(virtualHost);
-    connectionFactory.setPublisherReturns(true);
-    connectionFactory.setPublisherConfirms(true);
-    return connectionFactory;
-  }
-
-  @Bean
-  public RabbitTemplate rabbitTemplate(
-      ConnectionFactory connectionFactory,
-      DeliveryConfirmCallback deliveryConfirmCallback,
-      DeliveryReturnCallback deliveryReturnCallback,
-      Jackson2JsonMessageConverter jackson2MessageConverter) {
-    final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-    rabbitTemplate.setExchange(exchange);
-    rabbitTemplate.setRoutingKey(routingKey);
-    rabbitTemplate.setMessageConverter(jackson2MessageConverter);
-    rabbitTemplate.setMandatory(true);
-    rabbitTemplate.setConfirmCallback(deliveryConfirmCallback);
-    rabbitTemplate.setReturnCallback(deliveryReturnCallback);
-    return rabbitTemplate;
-  }
-
-  @Bean
-  public Jackson2JsonMessageConverter jackson2MessageConverter() {
-    return new Jackson2JsonMessageConverter();
-  }
-
-  @Bean
-  public DeliveryConfirmCallback deliveryConfirmCallback() {
-    return new DeliveryConfirmCallback();
-  }
-
-  @Bean
-  public DeliveryReturnCallback deliveryReturnCallback() {
-    return new DeliveryReturnCallback();
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
   }
 
   public static void main(String[] args) {
--- distros/spring/src/main/java/com/connexta/transformation/service/TransformController.java
+++ distros/spring/src/main/java/com/connexta/transformation/service/TransformController.java
@@ -13,14 +13,13 @@
  */
 package com.connexta.transformation.service;
 
-import com.connexta.transformation.TransformationService;
 import com.connexta.transformation.rest.models.TransformRequest;
-import com.connexta.transformation.rest.models.TransformResponse;
 import com.connexta.transformation.rest.springboot.TransformApi;
 import com.google.common.base.Preconditions;
 import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -29,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Implementation of the Transformation RESTful service. This is the main entry point for all HTTP
@@ -42,29 +42,40 @@ public class TransformController implements TransformApi {
 
   private static final String ACCEPT_VERSION = "Accept-Version";
 
-  private final TransformationService transformationService;
+  private final RestTemplate restTemplate;
 
-  public TransformController(TransformationService transformationService) {
-    Preconditions.checkNotNull(transformationService, "Transformation Service cannot be null.");
-    this.transformationService = transformationService;
+  private final String lookupServiceUrl;
+
+  public TransformController(
+      RestTemplate restTemplate, @Qualifier("lookupServiceUrl") String lookupServiceUrl) {
+    Preconditions.checkNotNull(restTemplate, "Rest Template cannot be null.");
+    Preconditions.checkNotNull(lookupServiceUrl, "Lookup Service Url cannot be null.");
+    this.restTemplate = restTemplate;
+    this.lookupServiceUrl = lookupServiceUrl;
   }
 
   /**
    * Handles application/json POST transform requests to the /transform context. This method
-   * handles: - Validating the message - Calling the transformation sevice to forward the request to
-   * the rquest queue - Returning an appropriate response to the caller
+   * handles: - Validating the message - Forwarding the request to the service lookup service.
+   * Returns an HTTP Status Code of 202 Accepted on success; otherwise, an error status code.
    */
   @RequestMapping(
       value = "/transform",
-      produces = {MediaType.APPLICATION_JSON_VALUE},
       consumes = {MediaType.APPLICATION_JSON_VALUE},
       method = RequestMethod.POST)
-  @Override
-  public ResponseEntity<TransformResponse> transform(
+  public ResponseEntity<Void> transform(
       @RequestHeader(ACCEPT_VERSION) String acceptVersion,
       @Valid @RequestBody TransformRequest transformRequest) {
     LOGGER.debug("{}: {}", ACCEPT_VERSION, acceptVersion);
-    TransformResponse transformResponse = transformationService.transform(transformRequest);
-    return ResponseEntity.accepted().body(transformResponse);
+    forwardRequest(transformRequest);
+    return ResponseEntity.accepted().build();
+  }
+
+  private void forwardRequest(TransformRequest transformRequest) {
+    LOGGER.debug("Forwarding request to: {}", lookupServiceUrl);
+    ResponseEntity<String> response =
+        restTemplate.postForEntity(lookupServiceUrl, transformRequest, String.class);
+    LOGGER.debug(
+        "HTTP response status code from lookup service: {}", response.getStatusCodeValue());
   }
 }
