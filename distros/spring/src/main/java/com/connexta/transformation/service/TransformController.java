austinsteffes/ion-transformation

LORE-254 Update POST endpoint logic

distros/spring/src/main/java/com/connexta/transformation/service/TransformController.java
@@ -13,13 +13,18 @@
  */
 package com.connexta.transformation.service;
 
+import com.connexta.transformation.commons.api.exceptions.TransformationException;
+import com.connexta.transformation.commons.api.status.Transformation;
+import com.connexta.transformation.commons.inmemory.InMemoryTransformationManager;
 import com.connexta.transformation.rest.models.TransformRequest;
-import com.connexta.transformation.rest.springboot.TransformApi;
+import com.connexta.transformation.rest.spring.TransformApiTransform;
 import com.google.common.base.Preconditions;
+import java.net.URI;
+import java.net.URISyntaxException;
 import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -28,36 +33,32 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
- * Implementation of the Transformation RESTful service. This is the main entry point for all HTTP
- * requests.
+ * Implementation of the Transformation RESTful POST/TRANSFORM service
  */
 @RestController
 @CrossOrigin(origins = "*")
-public class TransformController implements TransformApi {
+public class TransformController implements TransformApiTransform {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TransformController.class);
 
   private static final String ACCEPT_VERSION = "Accept-Version";
 
-  private final RestTemplate restTemplate;
+  private final InMemoryTransformationManager transformationManager;
 
-  private final String lookupServiceUrl;
-
-  public TransformController(
-      RestTemplate restTemplate, @Qualifier("lookupServiceUrl") String lookupServiceUrl) {
-    Preconditions.checkNotNull(restTemplate, "Rest Template cannot be null.");
-    Preconditions.checkNotNull(lookupServiceUrl, "Lookup Service Url cannot be null.");
-    this.restTemplate = restTemplate;
-    this.lookupServiceUrl = lookupServiceUrl;
+  @Autowired
+  public TransformController(InMemoryTransformationManager transformationManager) {
+    Preconditions.checkNotNull(transformationManager, "TransformationManager cannot be null");
+    this.transformationManager = transformationManager;
   }
 
   /**
    * Handles application/json POST transform requests to the /transform context. This method
    * handles: - Validating the message - Forwarding the request to the service lookup service.
-   * Returns an HTTP Status Code of 202 Accepted on success; otherwise, an error status code.
+   * Returns an HTTP Status Code of 201 Created on success and a location URI; otherwise, an error
+   * status code.
    */
   @RequestMapping(
       value = "/transform",
@@ -65,17 +66,27 @@ public class TransformController implements TransformApi {
       method = RequestMethod.POST)
   public ResponseEntity<Void> transform(
       @RequestHeader(ACCEPT_VERSION) String acceptVersion,
-      @Valid @RequestBody TransformRequest transformRequest) {
+      @Valid @RequestBody TransformRequest transformRequest)
+      throws URISyntaxException, TransformationException {
+
     LOGGER.debug("{}: {}", ACCEPT_VERSION, acceptVersion);
-    forwardRequest(transformRequest);
-    return ResponseEntity.accepted().build();
+    return postURI(transformRequest);
   }
 
-  private void forwardRequest(TransformRequest transformRequest) {
-    LOGGER.debug("Forwarding request to: {}", lookupServiceUrl);
-    ResponseEntity<String> response =
-        restTemplate.postForEntity(lookupServiceUrl, transformRequest, String.class);
-    LOGGER.debug(
-        "HTTP response status code from lookup service: {}", response.getStatusCodeValue());
+  private ResponseEntity<Void> postURI(TransformRequest transformRequest)
+      throws TransformationException, URISyntaxException {
+
+    ResponseEntity<Void> response;
+    Transformation trans =
+        transformationManager.createTransform(
+            transformRequest.getCurrentLocation(),
+            transformRequest.getFinalLocation(),
+            transformRequest.getMetacardLocation());
+
+    URI s = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+    URI uri = new URI(String.format("%s/%s", s.toString(), trans.getTransformId()));
+    response = ResponseEntity.created(uri).build();
+
+    return response;
   }
 }

distros/spring/src/test/java/com/connexta/transformation/service/TransformControllerTest.java
@@ -13,51 +13,121 @@
  */
 package com.connexta.transformation.service;
 
-import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.connexta.transformation.commons.api.status.Transformation;
+import com.connexta.transformation.commons.inmemory.InMemoryTransformationManager;
 import com.connexta.transformation.rest.models.TransformRequest;
-import org.junit.Assert;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.ResourceAccessException;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = "endpoints.lookupService.url = http://localhost:")
-@ContextConfiguration
+@WebMvcTest(TransformController.class)
 public class TransformControllerTest {
-  private static final String ACCEPT_VERSION = "0.1.0";
 
-  @LocalServerPort private int port;
+  private static final String TEST_URI = "http://test:9000";
 
-  @Autowired private TestRestTemplate restTemplate;
+  private static final String ACCEPT_VERSION = "Accept-Version";
 
-  @Autowired private String lookupServiceUrl;
+  private static final String ACCEPT_VERSION_NUM = "0.1.0";
+
+  private ObjectMapper mapper = new ObjectMapper();
+
+  @Autowired private MockMvc mockMvc;
+
+  @MockBean InMemoryTransformationManager manager;
 
   @Test
-  public void testAcceptedResponseEntity() {
-    TransformController controller =
-        new TransformController(restTemplate.getRestTemplate(), lookupServiceUrl + port);
-    ResponseEntity responseEntity = controller.transform(ACCEPT_VERSION, new TransformRequest());
+  public void testCreatedResponseEntity() throws Exception {
+    Transformation transformation = mock(Transformation.class);
+    when(transformation.getTransformId()).thenReturn("id123");
+    when(manager.createTransform(any(), any(), any())).thenReturn(transformation);
+    this.mockMvc
+        .perform(
+            post("/transform")
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    mapper.writeValueAsString(
+                        new TransformRequest()
+                            .currentLocation(new URI(TEST_URI))
+                            .finalLocation(new URI(TEST_URI))
+                            .metacardLocation(new URI(TEST_URI))))
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isCreated())
+        .andExpect(header().string("Location", "http://localhost/transform/id123"))
+        .andExpect(redirectedUrl("http://localhost/transform/id123"));
 
-    Assert.assertThat(responseEntity.getStatusCode(), is(HttpStatus.ACCEPTED));
   }
 
-  @Test(expected = ResourceAccessException.class)
-  public void testInvalidServiceUrl() {
-    String badServiceUrl = "http://bad.url";
-    TransformController controller =
-        new TransformController(restTemplate.getRestTemplate(), badServiceUrl);
+  @Test
+  public void testCurrentLocationCannotBeNull400() throws Exception {
+    this.mockMvc
+        .perform(
+            post("/transform")
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    mapper.writeValueAsString(
+                        new TransformRequest()
+                            .currentLocation(null)
+                            .finalLocation(new URI(TEST_URI))
+                            .metacardLocation(new URI(TEST_URI))))
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isBadRequest());
+  }
 
-    controller.transform(ACCEPT_VERSION, new TransformRequest());
+  @Test
+  public void testFinalLocationNullCannotBe400() throws Exception {
+    this.mockMvc
+        .perform(
+            post("/transform")
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    mapper.writeValueAsString(
+                        new TransformRequest()
+                            .currentLocation(new URI(TEST_URI))
+                            .finalLocation(null)
+                            .metacardLocation(new URI(TEST_URI))))
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testMetacardlLocationCannotBeNull400() throws Exception {
+    this.mockMvc
+        .perform(
+            post("/transform")
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    mapper.writeValueAsString(
+                        new TransformRequest()
+                            .currentLocation(new URI(TEST_URI))
+                            .finalLocation(new URI(TEST_URI))
+                            .metacardLocation(null)))
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testManagerCannotBeNull() {
+    new TransformController(null);
   }
 }

connexta/transformation

distros/spring/src/main/java/com/connexta/transformation/service/TransformController.java
@@ -14,85 +14,85 @@
 package com.connexta.transformation.service;
 
 import com.connexta.transformation.commons.api.TransformationManager;
+import com.connexta.transformation.commons.api.exceptions.TransformationException;
+import com.connexta.transformation.commons.api.status.Transformation;
 import com.connexta.transformation.rest.models.TransformRequest;
-import com.connexta.transformation.rest.springboot.TransformApi;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
-import io.swagger.annotations.ApiParam;
+import com.connexta.transformation.rest.spring.TransformApiTransform;
+import java.net.URI;
 import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-/**
- * Implementation of the Transformation RESTful service. This is the main entry point for all HTTP
- * requests.
- */
+/** Implementation of the Transformation RESTful POST/TRANSFORM service */
 @RestController
 @CrossOrigin(origins = "*")
-public class TransformController implements TransformApi {
+public class TransformController implements TransformApiTransform {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TransformController.class);
 
   private static final String ACCEPT_VERSION = "Accept-Version";
 
-  private final RestTemplate restTemplate;
+  private final TransformationManager transformationManager;
 
-  private final String lookupServiceUrl;
-
-  public TransformationManager transformationManager;
-
+  /**
+   * Constructor that initializes the transformation manager
+   *
+   * @param transformationManager - Provides the ability to save transformation request info,
+   *     retrieve objects containing * transformation status and results, and delete data associated
+   *     with a transformation
+   */
   @Autowired
-  public TransformController(
-      RestTemplateBuilder builder,
-      @Qualifier("lookupServiceUrl") String lookupServiceUrl,
-      TransformationManager transformManager) {
-    this(builder.build(), lookupServiceUrl);
-    this.transformationManager = transformManager;
+  public TransformController(TransformationManager transformationManager) {
+    this.transformationManager = transformationManager;
   }
 
-  @VisibleForTesting
-  TransformController(
-      RestTemplate restTemplate, @Qualifier("lookupServiceUrl") String lookupServiceUrl) {
-    Preconditions.checkNotNull(restTemplate, "Rest Template cannot be null.");
-    Preconditions.checkNotNull(lookupServiceUrl, "Lookup Service Url cannot be null.");
-    this.restTemplate = restTemplate;
-    this.lookupServiceUrl = lookupServiceUrl;
-  }
-
+  /**
+   * Handles application/json POST transform requests to the /transform context. This method
+   * handles: - Validating the message - Forwarding the request to the service lookup service.
+   * Returns an HTTP Status Code of 201 Created on success and a location URI; otherwise, an error
+   * status code.
+   *
+   * @param acceptVersion - The API version used by the client to produce the REST message
+   * @param transformRequest - A request to transform a file into discovery metadata and other
+   *     supporting products
+   * @throws TransformationException if an error occurs while executing this method
+   * @return response of the request and the URI for polling the status is returned in the Location
+   *     header of the response.
+   */
   @Override
   public ResponseEntity<Void> transform(
-      @ApiParam(
-              value =
-                  "The API version used by the client to produce the REST message. The client must accept responses marked with any minor versions after this one. This implies that all future minor versions of the message are backward compatible with all previous minor versions of the same message. ",
-              required = true)
-          @RequestHeader(value = "Accept-Version", required = true)
-          String acceptVersion,
-      @ApiParam(
-              value =
-                  "A request to transform a file into discovery metadata and other supporting products.",
-              required = true)
-          @Valid
-          @RequestBody
-          TransformRequest transformRequest) {
+      @RequestHeader(ACCEPT_VERSION) String acceptVersion,
+      @Valid @RequestBody TransformRequest transformRequest)
+      throws TransformationException {
+
     LOGGER.debug("{}: {}", ACCEPT_VERSION, acceptVersion);
-    forwardRequest(transformRequest);
-    return ResponseEntity.accepted().build();
+    return postURI(transformRequest);
   }
 
-  private void forwardRequest(TransformRequest transformRequest) {
-    LOGGER.debug("Forwarding request to: {}", lookupServiceUrl);
-    ResponseEntity<String> response =
-        restTemplate.postForEntity(lookupServiceUrl, transformRequest, String.class);
-    LOGGER.debug(
-        "HTTP response status code from lookup service: {}", response.getStatusCodeValue());
+  private ResponseEntity<Void> postURI(TransformRequest transformRequest)
+      throws TransformationException {
+
+    ResponseEntity<Void> response;
+    Transformation trans =
+        transformationManager.createTransform(
+            transformRequest.getCurrentLocation(),
+            transformRequest.getFinalLocation(),
+            transformRequest.getMetacardLocation());
+
+    URI uri =
+        ServletUriComponentsBuilder.fromCurrentRequest()
+            .pathSegment(trans.getTransformId())
+            .build()
+            .toUri();
+    response = ResponseEntity.created(uri).build();
+
+    return response;
   }
 }

distros/spring/src/test/java/com/connexta/transformation/service/TransformControllerTest.java
@@ -13,25 +13,30 @@
  */
 package com.connexta.transformation.service;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.connexta.transformation.commons.api.TransformationManager;
+import com.connexta.transformation.commons.api.status.Transformation;
 import com.connexta.transformation.rest.models.TransformRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import org.json.JSONObject;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Answers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.ResourceAccessException;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(TransformController.class)
@@ -39,24 +44,34 @@ public class TransformControllerTest {
 
   private static final String TEST_URI = "http://test:9000";
 
+  private static final String TEST_ID = "id123";
+
   private static final String ACCEPT_VERSION = "Accept-Version";
 
   private static final String ACCEPT_VERSION_NUM = "0.1.0";
+
+  private static final String CURRENT_LOCATION = "currentLocation";
+
+  private static final String FINAL_LOCATION = "finalLocation";
+
+  private static final String METACARD_LOCATION = "metacardLocation";
+
+  private static final String TRANSFORM = "/transform";
 
   private ObjectMapper mapper = new ObjectMapper();
 
   @Autowired private MockMvc mockMvc;
 
-  @MockBean TransformationManager tManager;
-
-  @MockBean(answer = Answers.RETURNS_MOCKS)
-  private RestTemplateBuilder restTemplateBuilder;
+  @MockBean TransformationManager manager;
 
   @Test
-  public void testAcceptedResponseEntity() throws Exception {
+  public void testCreatedResponseEntity() throws Exception {
+    Transformation transformation = mock(Transformation.class);
+    when(transformation.getTransformId()).thenReturn(TEST_ID);
+    when(manager.createTransform(any(), any(), any())).thenReturn(transformation);
     this.mockMvc
         .perform(
-            post("/transform")
+            post(TRANSFORM)
                 .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(
@@ -66,15 +81,143 @@ public class TransformControllerTest {
                             .finalLocation(new URI(TEST_URI))
                             .metacardLocation(new URI(TEST_URI))))
                 .characterEncoding(StandardCharsets.UTF_8.name()))
-        .andExpect(status().isAccepted());
+        .andExpect(status().isCreated())
+        .andExpect(header().string("Location", "http://localhost/transform/id123"))
+        .andExpect(redirectedUrl("http://localhost/transform/id123"));
   }
 
-  @Test(expected = ResourceAccessException.class)
-  public void testInvalidServiceUrl() {
-    String badServiceUrl = "http://bad.url";
-    TransformController controller =
-        new TransformController(new RestTemplateBuilder(), badServiceUrl, tManager);
+  // This be a 400, but we use URI instead of URL in the openAPI spec. Thus, 23 is a valid URI while
+  // it wouldn't be a valid URL and gives status of 201.
+  @Ignore
+  public void testInvalidCurrentLocationBody() throws Exception {
+    Transformation transformation = mock(Transformation.class);
+    when(transformation.getTransformId()).thenReturn(TEST_ID);
+    when(manager.createTransform(any(), any(), any())).thenReturn(transformation);
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put(CURRENT_LOCATION, 23);
+    jsonObject.put(FINAL_LOCATION, TEST_URI);
+    jsonObject.put(METACARD_LOCATION, TEST_URI);
+    this.mockMvc
+        .perform(
+            post(TRANSFORM)
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonObject.toString())
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isBadRequest());
+  }
 
-    controller.transform(ACCEPT_VERSION, new TransformRequest());
+  @Test
+  public void testOmitCurrentLocation400() throws Exception {
+    Transformation transformation = mock(Transformation.class);
+    when(transformation.getTransformId()).thenReturn(TEST_ID);
+    when(manager.createTransform(any(), any(), any())).thenReturn(transformation);
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put(FINAL_LOCATION, TEST_URI);
+    jsonObject.put(METACARD_LOCATION, TEST_URI);
+    this.mockMvc
+        .perform(
+            post(TRANSFORM)
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonObject.toString())
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testOmitFinalLocation400() throws Exception {
+    Transformation transformation = mock(Transformation.class);
+    when(transformation.getTransformId()).thenReturn(TEST_ID);
+    when(manager.createTransform(any(), any(), any())).thenReturn(transformation);
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put(CURRENT_LOCATION, TEST_URI);
+    jsonObject.put(METACARD_LOCATION, TEST_URI);
+    this.mockMvc
+        .perform(
+            post(TRANSFORM)
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonObject.toString())
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testOmitMetacardLocation400() throws Exception {
+    Transformation transformation = mock(Transformation.class);
+    when(transformation.getTransformId()).thenReturn(TEST_ID);
+    when(manager.createTransform(any(), any(), any())).thenReturn(transformation);
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put(CURRENT_LOCATION, TEST_URI);
+    jsonObject.put(FINAL_LOCATION, TEST_URI);
+    this.mockMvc
+        .perform(
+            post(TRANSFORM)
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonObject.toString())
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testCurrentLocationCannotBeNull400() throws Exception {
+    Transformation transformation = mock(Transformation.class);
+    when(transformation.getTransformId()).thenReturn(TEST_ID);
+    when(manager.createTransform(any(), any(), any())).thenReturn(transformation);
+    this.mockMvc
+        .perform(
+            post(TRANSFORM)
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    mapper.writeValueAsString(
+                        new TransformRequest()
+                            .currentLocation(null)
+                            .finalLocation(new URI(TEST_URI))
+                            .metacardLocation(new URI(TEST_URI))))
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testFinalLocationNullCannotBe400() throws Exception {
+    Transformation transformation = mock(Transformation.class);
+    when(transformation.getTransformId()).thenReturn(TEST_ID);
+    when(manager.createTransform(any(), any(), any())).thenReturn(transformation);
+    this.mockMvc
+        .perform(
+            post(TRANSFORM)
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    mapper.writeValueAsString(
+                        new TransformRequest()
+                            .currentLocation(new URI(TEST_URI))
+                            .finalLocation(null)
+                            .metacardLocation(new URI(TEST_URI))))
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testMetacardlLocationCannotBeNull400() throws Exception {
+    Transformation transformation = mock(Transformation.class);
+    when(transformation.getTransformId()).thenReturn(TEST_ID);
+    when(manager.createTransform(any(), any(), any())).thenReturn(transformation);
+    this.mockMvc
+        .perform(
+            post(TRANSFORM)
+                .header(ACCEPT_VERSION, ACCEPT_VERSION_NUM)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    mapper.writeValueAsString(
+                        new TransformRequest()
+                            .currentLocation(new URI(TEST_URI))
+                            .finalLocation(new URI(TEST_URI))
+                            .metacardLocation(null)))
+                .characterEncoding(StandardCharsets.UTF_8.name()))
+        .andExpect(status().isBadRequest());
   }
 }